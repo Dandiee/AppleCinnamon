7d060b7fe511515512f244bdd165899981f26253
Daniel Koczur <dkoczur@slb.com>
2023-04-30 00:32:01 +0200

    better

--- Tools/NoiseGeneratorTest/NoiseGeneratorTest/MainWindow.xaml.cs
+++ Tools/NoiseGeneratorTest/NoiseGeneratorTest/MainWindow.xaml.cs
@@ -1,57 +1,238 @@
-﻿namespace NoiseGeneratorTest
+﻿
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace NoiseGeneratorTest
 {
     public partial class MainWindow
     {
-        //private readonly Device _device;
-        //private Surface _target;
-        //private Surface _surface;
-
-        //private static readonly Duration Timeout = new(TimeSpan.FromSeconds(1));
-
         public MainWindowViewModel ViewModel { get; }
 
         public MainWindow()
         {
             InitializeComponent();
 
-            //_device = new Device(new Direct3D(), 0, DeviceType.Hardware, new WindowInteropHelper(this).Handle, CreateFlags.HardwareVertexProcessing, new PresentParameters(1, 1));
-            //_target = Surface.CreateRenderTarget(_device, _width, _height, Format.A8R8G8B8, MultisampleType.None, 0, true);
-            //_surface = Surface.CreateOffscreenPlain(_device, _width, _height, Format.A8R8G8B8, Pool.SystemMemory);
-
             ViewModel = new MainWindowViewModel(this);
             DataContext = ViewModel;
+
+            var pf = new PathFigure();
+            var cb = new CubicBezier(MyCanvas, 
+                new Point(100, 100), 
+                new Point(150, 50), 
+                new Point(150, 150), 
+                new Point(200, 100),
+                pf);
+            cb.LinkTo(new Point(250, 50), new Point(250, 150), new Point(300, 100));
+
+            pf.StartPoint = cb.Start.Point;
+            pf.Segments = new PathSegmentCollection(cb.GetSegments());
+
+            var pfc = new PathFigureCollection(new[] { pf });
+            var pg = new PathGeometry(pfc);
+
+            MyPath.Data = pg;
         }
 
-        private int _width = 256;
-        private int _height = 256;
+    }
 
-        public void Draw(ref byte[] bytes, int width, int height)
+    public class CubicBezier
+    {
+        public Canvas Canvas { get; set; }
+
+        public ControlPoint Cp1 { get; set; }
+        public ControlPoint Cp2 { get; set; }
+
+        public EndPoint End { get; set; }
+        public EndPoint Start { get; set; }
+
+        public CubicBezier Prev { get; set; }
+        public CubicBezier Next { get; set; }
+
+        public BezierSegment Segment { get; set; }
+        public PathFigure PathFigure { get; set; }
+
+        public Line Cl1 { get; set; }
+        public Line Cl2 { get; set; }
+
+
+        public CubicBezier(Canvas canvas, Point start, Point cp1, Point cp2, Point end, PathFigure pathFigure = null)
         {
-            //MainImage.Draw(ref bytes, width, height);
-            //MainImage2.Draw(ref bytes, width, height);
+            PathFigure = pathFigure;
+            Canvas = canvas;
 
-            //if (!NoiseD3DImage.TryLock(Timeout))
-            //{
-            //    throw new Exception("Sorry mate");
-            //}
+            Cl1 = new Line
+            {
+                Stroke = new SolidColorBrush(Colors.Orange),
+                StrokeThickness = 1,
+                //StrokeDashCap = PenLineCap.Flat
+            };
 
-            //if (_width != width || _height != height)
-            //{
-            //    _width = width;
-            //    _height = height;
+            Cl2 = new Line
+            {
+                Stroke = new SolidColorBrush(Colors.Orange),
+                StrokeThickness = 1,
+                //StrokeDashCap = PenLineCap.Flat
+            };
 
-            //    _target.Dispose();
-            //    _target = Surface.CreateRenderTarget(_device, _width, _height, Format.A8R8G8B8, MultisampleType.None, 0, true);
+            Canvas.Children.Add(Cl1);
+            Canvas.Children.Add(Cl2);
 
-            //    _surface.Dispose();
-            //    _surface = Surface.CreateOffscreenPlain(_device, _width, _height, Format.A8R8G8B8, Pool.SystemMemory);
-            //}
 
-            //Surface.FromMemory(_surface, bytes, Filter.None, 0, Format.A8R8G8B8, 4 * _width, new RawRectangle(0, 0, _width, _height));
-            //_device.UpdateSurface(_surface, _target);
-            //NoiseD3DImage.SetBackBuffer(D3DResourceType.IDirect3DSurface9, _target.NativePointer);
-            //NoiseD3DImage.AddDirtyRect(new Int32Rect(0, 0, NoiseD3DImage.PixelWidth, NoiseD3DImage.PixelHeight));
-            //NoiseD3DImage.Unlock();
+            Start = new EndPoint(start, Canvas, this);
+            Cp1 = new ControlPoint(cp1, Canvas, this);
+            End = new EndPoint(end, Canvas, this);
+            Cp2 = new ControlPoint(cp2, Canvas, this);
+
+            
+        }
+
+        public CubicBezier LinkTo(Point cp1, Point cp2, Point end)
+        {
+            var segment = new CubicBezier(Canvas, End.Point, cp1, cp2, end);
+            segment.Prev = this;
+            Next = segment;
+            return segment;
+        }
+
+        public IEnumerable<PathSegment> GetSegments()
+        {
+            var s = this;
+            while (s != null)
+            {
+                //var q = new BezierSegment();
+                //var binding = new Binding(nameof(Cp1.Point))
+                //{
+                //    Source = s
+                //};
+                //q.Set
+                var segment = new BezierSegment(s.Cp1.Point, s.Cp2.Point, s.End.Point, true);
+                s.Segment = segment;
+                s.Update();
+                s = s.Next;
+
+                yield return segment;
+            }
+        }
+
+        public void Update()
+        {
+            if (Segment == null) return;
+            Segment.Point1 = Cp1.Point;
+            Segment.Point2 = Cp2.Point;
+            Segment.Point3 = End.Point;
+
+            if (Prev != null)
+            {
+                Cl1.X1 = Prev.End.Point.X;
+                Cl1.Y1 = Prev.End.Point.Y;
+            }
+            else
+            {
+                Cl1.X1 = Start.Point.X;
+                Cl1.Y1 = Start.Point.Y;
+
+                PathFigure.StartPoint = Start.Point;
+            }
+            
+            Cl1.X2 = Cp1.Point.X;
+            Cl1.Y2 = Cp1.Point.Y;
+
+            Cl2.X1 = End.Point.X;
+            Cl2.Y1 = End.Point.Y;
+            Cl2.X2 = Cp2.Point.X;
+            Cl2.Y2 = Cp2.Point.Y;
+
+            if (Next != null)
+            {
+                Next.Update();
+            }
+
+        }
+    }
+
+    public class BezierPoint
+    {
+        public readonly CubicBezier Owner;
+        public Canvas Canvas { get; set; }
+        public Point Point { get; set; }
+        public Ellipse Elipse { get; set; }
+        public SolidColorBrush Brush { get; set; }
+        public double Radius { get; set; }
+
+        private bool _isDragging;
+        private Point _startDragPosition;
+
+        public BezierPoint(Color color, Point point, Canvas canvas, CubicBezier owner)
+        {
+            Owner = owner;
+            Canvas = canvas;
+            Point = point;
+            Brush = new SolidColorBrush(color);
+            Radius = 10;
+
+            Elipse = new Ellipse
+            {
+                Fill = Brush,
+                Width = Radius,
+                Height = Radius
+            };
+
+            Elipse.MouseDown += (_, e) =>
+            {
+                _startDragPosition = e.GetPosition(Canvas);
+                _isDragging = true;
+            };
+
+            Elipse.MouseUp += (_, _) =>
+            {
+                _isDragging = false;
+            };
+
+            Canvas.MouseMove    += (_, e) =>
+            {
+                if (_isDragging)
+                {
+                    var currentPosition = e.GetPosition(Canvas);
+                    var delta = currentPosition - _startDragPosition;
+
+                    var left = Canvas.GetLeft(Elipse);
+                    var top = Canvas.GetTop(Elipse);
+
+                    Canvas.SetLeft(Elipse, left + delta.X);
+                    Canvas.SetTop(Elipse, top + delta.Y);
+
+                    Point = Point + delta;
+
+                    Owner.Update();
+
+                    _startDragPosition = currentPosition;
+                }
+            };
+
+            Canvas.SetLeft(Elipse, point.X - Radius / 2f);
+            Canvas.SetTop(Elipse, point.Y - Radius / 2f);
+
+            Canvas.Children.Add(Elipse);
+        }
+    }
+
+    public class ControlPoint : BezierPoint
+    {
+        public ControlPoint(Point point, Canvas canvas, CubicBezier owner)
+         : base(Colors.Red, point, canvas, owner)
+        {
+
+        }
+    }
+
+    public class EndPoint : BezierPoint
+    {
+        public EndPoint(Point point, Canvas canvas, CubicBezier owner)
+            : base(Colors.Blue, point, canvas, owner)
+        {
         }
     }
 }
